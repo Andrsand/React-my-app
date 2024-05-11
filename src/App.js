02718ef9c45f89ae155020854ff70a1916ab0bf6
andrsand <angorko@list.ru>
2024-05-11 08:17:03 +0300

    Example of event response

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-const listItems = products.map(product =>
-  <li
-    key={product.id}
-    style={{
-      color: product.isFruit ? 'magenta' : 'darkgreen'
-    }}
-  >
-    {product.title}
-  </li>
-);
 
 function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
   return (
-    <button>I'm a button</button>
+    <button onClick={handleClick}>
+      Click me
+    </button>
   );
 }
 
@@ -32,7 +22,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <ul>{listItems}</ul>
+
         <MyButton />
         <a
           className="App-link"
