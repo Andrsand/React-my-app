63f8b11b75b39c2451714af1d0a85543579c0545
andrsand <angorko@list.ru>
2024-08-04 23:45:21 +0300

    added comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// Функциональный компонент React для рендеринга отдельного квадрата на доске
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
@@ -9,16 +10,23 @@ function Square({ value, onSquareClick }) {
 }
 
 function Board({ xIsNext, squares, onPlay }) {
+  // Функция обработки события клика по квадрату
   function handleClick(i) {
+    // Проверяем, был ли определен победитель или квадрат уже был выбран
+    // Если любое из этих условий выполняется, то возвращаемся, чтобы избежать дальнейших действий
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
+
+    // Создаем копию массива squares, чтобы избежать мутации оригинального массива
     const nextSquares = squares.slice();
+    // В зависимости оттого, чей ход, устанавливаем значение квадрата как 'X' или 'O'
     if (xIsNext) {
       nextSquares[i] = 'X';
     } else {
       nextSquares[i] = 'O';
     }
+    // Вызываем функцию onPlay, чтобы обновить состояние игры с новым массивом squares
     onPlay(nextSquares);
   }
 
@@ -94,23 +102,35 @@ export default function Game() {
   );
 }
 
+// Функция calculateWinner принимает массив squares, представляющий текущее состояние игрового поля 3x3.
+
 function calculateWinner(squares) {
+  // Массив lines содержит все возможные комбинации выигрышных линий в игре крестики-нолики на доске 3x3.
   const lines = [
-    [0, 1, 2],
+    [0, 1, 2], // горизонтальные линии
     [3, 4, 5],
     [6, 7, 8],
-    [0, 3, 6],
+    [0, 3, 6], // вертикальные линии
     [1, 4, 7],
     [2, 5, 8],
-    [0, 4, 8],
+    [0, 4, 8], // диагональные линии
     [2, 4, 6],
   ];
+
+  // Перебираем все возможные выигрышные линии.
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
+    const [a, b, c] = lines[i];              // Для текущей линии извлекаем индексы элементов
+
+    // Проверяем, что элементы в данных индексах не являются пустыми (null, undefined, false, 0, пустая строка)
+    // и все три элемента равны между собой.
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return squares[a]; // Возвращаем символ (X или O), который образует выигрышную линию.
     }
   }
+
+  // Если ни одна из выигрышных линий не выполнена, возвращаем null, что означает отсутствие победителя.
+
   return null;
 }
 
